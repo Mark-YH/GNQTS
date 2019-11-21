0c6a39b1c8cba7d7de28466f60b0a90b17a95931
Mark Hsu <mark50404@gmail.com>
2019-11-21 12:58:37 +0800

    added a function that could dynamical set-up precision

--- Logger.h
+++ Logger.h
@@ -21,18 +21,30 @@ using std::exception;
 class Logger {
 private:
     ofstream fOut;
+    int precision;
 public:
     Logger(const string &path) {
         try {
             fOut.open(path, ios::app);
+            this->precision = 20;
         } catch (exception &e) {
             cerr << "Got an exception: " << e.what() << endl;
         }
     }
 
-    Logger(const string &path, ios_base::openmode mode) {
+    Logger(const string &path, int precision) {
+        try {
+            fOut.open(path, ios::app);
+            this->precision = precision;
+        } catch (exception &e) {
+            cerr << "Got an exception: " << e.what() << endl;
+        }
+    }
+
+    Logger(const string &path, ios_base::openmode mode, int precision) {
         try {
             fOut.open(path, mode);
+            this->precision = precision;
         } catch (exception &e) {
             cerr << "Got an exception: " << e.what() << endl;
         }
@@ -44,27 +56,27 @@ public:
 
     template<class T>
     void write(T num) {
-        fOut << setprecision(20) << num;
+        fOut << setprecision(precision) << num;
     }
 
     template<class T>
     void writeComma(T num) {
-        fOut << setprecision(20) << num << ',';
+        fOut << setprecision(precision) << num << ',';
     }
 
     template<class T>
     void writeTab(T num) {
-        fOut << setprecision(20) << num << '\t';
+        fOut << setprecision(precision) << num << '\t';
     }
 
     template<class T>
     void writeSpace(T num) {
-        fOut << setprecision(20) << num << ' ';
+        fOut << setprecision(precision) << num << ' ';
     }
 
     template<class T>
     void writeLine(T num) {
-        fOut << setprecision(20) << num << endl;
+        fOut << setprecision(precision) << num << endl;
     }
 };
 
