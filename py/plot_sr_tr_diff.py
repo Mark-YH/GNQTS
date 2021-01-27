2884b17a74fdb3017adf7e070eec0287586951a8
Mark-YH <mark50404@gmail.com>
2021-01-27 20:34:18 +0800

    added emotion index concept figure

--- py/plot_sr_tr_diff.py
+++ py/plot_sr_tr_diff.py
@@ -26,21 +26,32 @@ def get_return_trendline(fs):
 def sr():
     plt.figure(figsize=(7.2, 4.8))
     x = np.arange(0, 101, 1)
-    y = np.arange(50, 151, 1)
+    y1 = np.arange(50, 151, 1)
+    y2 = np.arange(150, 49, -1)
     avg_line = np.zeros(len(x)) + 100
-    lower_err = y - avg_line
+    lower_err1 = y1 - avg_line
+    lower_err2 = y2 - avg_line
     upp_err = [0] * len(x)
-    err = [lower_err, upp_err]
-    eb = plt.errorbar(x, y, yerr=err, errorevery=10, ecolor='tab:red', linewidth=0, elinewidth=2)
-    eb[-1][0].set_linestyle('--')
-    plt.plot(x, y, c='tab:blue', linewidth=2, label='Stable uptrend portfolio')
-    plt.plot(x, avg_line, c='tab:green', linewidth=2, label='Average')
+    err1 = [lower_err1, upp_err]
+    err2 = [lower_err2, upp_err]
+    eb1 = plt.errorbar(x, y1, yerr=err1, errorevery=10, ecolor='tab:green', linewidth=0, elinewidth=2)
+    eb2 = plt.errorbar(x, y2, yerr=err2, errorevery=10, ecolor='tab:blue', linewidth=0, elinewidth=2)
+    eb1[-1][0].set_linestyle('--')
+    eb2[-1][0].set_linestyle('--')
+    plt.plot(x, y1, c='tab:green', linewidth=2, label='Uptrend portfolio')
+    plt.plot(x, y2, c='tab:blue', linewidth=2, label='Downtrend portfolio')
+    plt.plot(x, avg_line, c='tab:orange', linewidth=2, label='Average line')
 
-    plt.annotate(s="Risk", xy=(70, 110), xytext=(60, 80),
+    plt.annotate(s="Risk", xy=(30, 90), xytext=(45, 80),
                  arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=-.2"), fontsize=18, c='tab:red')
-    plt.annotate(s="Risk", xy=(40, 95), xytext=(60, 80),
+    plt.annotate(s="Risk", xy=(70, 90), xytext=(45, 80),
                  arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"), fontsize=18, c='tab:red')
-    show('sr_explanation.pdf')
+    plt.xlabel('Day', fontsize=14)
+    plt.ylabel('Daily return', fontsize=14)
+    plt.legend(fontsize=14, loc='upper center')
+    plt.tight_layout()
+    plt.savefig('./py_output/sr_explanation.pdf')
+    plt.show()
 
 
 def tr():
@@ -65,18 +76,47 @@ def tr():
                  arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"), fontsize=18, c='tab:red')
     plt.annotate(s="Risk", xy=(8, 135), xytext=(6.5, 80),
                  arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=-.1"), fontsize=18, c='tab:red')
-    show('tr_explanation.pdf')
-
-
-def show(filename, x_label='Day', y_label='Daily return'):
-    plt.xlabel(x_label, fontsize=14)
-    plt.ylabel(y_label, fontsize=14)
-    plt.legend(fontsize=14)
+    plt.xlabel('Day', fontsize=14)
+    plt.ylabel('Funds standardization', fontsize=14)
+    plt.legend(fontsize=14, loc='upper center')
     plt.tight_layout()
-    plt.savefig('./py_output/' + filename)
+    plt.savefig('./py_output/tr_explanation.pdf')
+    plt.show()
+
+
+def ei():
+    plt.figure(figsize=(7.2, 4.8))
+    x = np.arange(0, 11, 1)
+    y = np.arange(50, 151, 10)
+    for i in range(len(y)):
+        if i < 5:
+            y[i] = y[i] + 100 * np.power(-1, i)
+        else:
+            y[i] = y[i] + 30 * np.power(-1, i)
+    y[0] = y[0] - 100
+    y[-1] = y[-1] - 30
+    first2last_line = np.arange(50, 151, 10)
+    lower_err = y - first2last_line
+    upp_err = [0] * len(x)
+    err = [lower_err, upp_err]
+    eb = plt.errorbar(x, y, yerr=err, errorevery=1, ecolor='tab:red', linewidth=0, elinewidth=2)
+    eb[-1][0].set_linestyle('--')
+    plt.plot(x, y, c='tab:blue', linewidth=2, label='Portfolio')
+    plt.plot(x, first2last_line, c='tab:green', linewidth=2, label='First-to-last line')
+
+    plt.annotate(s="", xy=(3, 45), xytext=(3.8, 37),
+                 arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"), fontsize=18, c='tab:red')
+    plt.annotate(s="Emotional ups and downs", xy=(4, 135), xytext=(3.8, 25),
+                 arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=-.3"), fontsize=18, c='tab:red')
+    plt.xlabel('Day', fontsize=14)
+    plt.ylabel('Funds standardization', fontsize=14)
+    plt.legend(fontsize=14, loc='best')
+    plt.tight_layout()
+    plt.savefig('./py_output/ei_explanation.pdf')
     plt.show()
 
 
 if __name__ == '__main__':
     sr()
     tr()
+    ei()
