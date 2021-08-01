49082896255694332892e967ea9b2a5e2a8cd9f7
Mark Hsu <mark50404@gmail.com>
2021-08-01 23:56:43 +0800

    applied group bar chart.

--- py/plot_result.py
+++ py/plot_result.py
@@ -13,7 +13,7 @@ objective_number = 3
 
 
 def read_training_period():
-    p = 'C:/Users/Lab114/Desktop/DJI30 convergence/result/TR vs SR training period.csv'
+    p = '/Users/Mark/Desktop/result/TR vs SR training period.csv'
     ANGQTS_EWFA = {'return': [], 'risk': [], 'tr': []}
     ANGQTS_FA = {'return': [], 'risk': [], 'tr': []}
     ANGQTS_SR = {'return': [], 'risk': [], 'tr': []}
@@ -46,7 +46,7 @@ def read_training_period():
 
 
 def read_testing_period():
-    p = 'C:/Users/Lab114/Desktop/DJI30 convergence/result/DJI30 ANGQTS-EIfixed/TR vs SR total testing period.csv'
+    p = '/Users/Mark/Desktop/result/DJI30 ANGQTS-EIfixed/TR vs SR total testing period.csv'
     ANGQTS_EWFA = {'profit': [], 'fluc': [], 'ei': [], 'return': [], 'risk': [], 'tr': []}
     ANGQTS_FA = {'profit': [], 'fluc': [], 'ei': [], 'return': [], 'risk': [], 'tr': []}
     ANGQTS_SR = {'profit': [], 'fluc': [], 'ei': [], 'return': [], 'risk': [], 'tr': []}
@@ -94,9 +94,11 @@ def read_testing_period():
         plot_bar('Overall Testing Period', 'Emotion Index', 'Testing period/Emotion index', data, sort_descend=True)
         t = 'return'
         data = [ANGQTS_EWFA[t], ANGQTS_FA[t], ANGQTS_SR[t]]
-        plot('Overall Testing Period', 'Daily Expected Return', 'Testing period/Expected return', data, sort_descend=True)
+        plot('Overall Testing Period', 'Daily Expected Return', 'Testing period/Expected return', data,
+             sort_descend=True)
         data = [ANGQTS_FA[t], DJIA[t]]
-        plot_bar('Overall Testing Period', 'Daily Expected Return', 'Testing period/Expected return', data, sort_descend=True)
+        plot_bar('Overall Testing Period', 'Daily Expected Return', 'Testing period/Expected return', data,
+                 sort_descend=True)
         t = 'risk'
         data = [ANGQTS_EWFA[t], ANGQTS_FA[t], ANGQTS_SR[t]]
         plot('Overall Testing Period', 'Risk', 'Testing period/Risk', data, sort_descend=False)
@@ -118,6 +120,15 @@ def sort(data, sorted_indices, sort_descend):
     return sorted_data
 
 
+def set_bar_label(ax, rects, labels):
+    height = ax.dataLim.height * 0.01
+    for rect, label in zip(rects, labels):
+        if label < 5:
+            ax.text(rect.get_x() + 0.025, height, '{:.5f}'.format(label), rotation=90,alpha=0.7)
+        else:
+            ax.text(rect.get_x() + 0.025, height, '{:.1f}'.format(label), rotation=90,alpha =0.7)
+
+
 def plot(title, y_label, filename, data, sort_descend=None):
     col_count = 13
     index = np.arange(0, col_count, 1)
@@ -136,16 +147,21 @@ def plot(title, y_label, filename, data, sort_descend=None):
     plt.rcParams['font.family'] = "Times New Roman"
     fig = plt.figure(figsize=(7.2, 4.8))
     ax = fig.add_subplot(1, 1, 1)
-    ax.plot(ANGQTS_EWFA, alpha=.8, label="ANGQTS-EWFA", marker='x')
-    ax.plot(ANGQTS_FA, alpha=.8, label="ANGQTS-NEWFA", marker='o')
-    ax.plot(ANGQTS_SR, alpha=.8, label="ANGQTS-SR", marker='+')
+    width = 0.3
+    index = np.arange(0, 13, 1)
+    rects1 = ax.bar(index - width, ANGQTS_EWFA, width, alpha=.8, label="ANGQTS-EWFA", align='center')
+    rects2 = ax.bar(index, ANGQTS_FA, width, alpha=.8, label="ANGQTS-FA", align='center')
+    rects3 = ax.bar(index + width, ANGQTS_SR, width, alpha=.8, label="ANGQTS-SR", align='center')
 
+    set_bar_label(ax, rects1, ANGQTS_EWFA)
+    set_bar_label(ax, rects2, ANGQTS_FA)
+    set_bar_label(ax, rects3, ANGQTS_SR)
     ax.set_ylabel(y_label, fontsize=14)
     ax.set_xticklabels(xlabels)
     ax.xaxis.set_major_locator(ticker.FixedLocator(index))
 
-    plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
-    plt.legend(fontsize=14, ncol=1)
+    ax.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+    ax.legend(fontsize=14, ncol=1)
     plt.title(title, fontsize=14)
     plt.tight_layout()
     file_path = './py_output/'
