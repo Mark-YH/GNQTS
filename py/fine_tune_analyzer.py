debcc51b39cf1e5390dc4c5ba3d1752b720e3f81
Mark Hsu <mark50404@gmail.com>
2021-09-22 15:24:56 +0800

    fixed a bug of regex format and added plotting function

--- py/fine_tune_analyzer.py
+++ py/fine_tune_analyzer.py
@@ -7,6 +7,9 @@ Created on 2021/9/21
 
 import os
 import re
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
 
 
 def get_files():
@@ -42,13 +45,67 @@ def get_tr(fp):
         return reader.readline().split(',')[4]
 
 
-if __name__ == '__main__':
+def d4(x, y, z, u):
+    matplotlib.use('macosx')
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")
+    img = ax.scatter(x, y, z, c=u, cmap=plt.jet(), alpha=0.7, marker='x')
+    ax.set_xlabel(r'$\theta_u$')
+    ax.set_ylabel(r'$\theta_l$')
+    ax.set_zlabel('Times')
+    fig.colorbar(img, pad=0.1)
+    fig.tight_layout()
+    plt.show()
+
+
+def d3(x, y, z):
+    matplotlib.use('macosx')
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")
+    img = ax.scatter(x, y, z, c=z, cmap=plt.jet(), alpha=0.7, marker='x')
+    ax.set_xlabel(r'$\theta_u$')
+    ax.set_ylabel(r'$\theta_l$')
+    ax.set_zlabel('Trend ratio')
+    fig.colorbar(img, pad=0.1)
+    fig.tight_layout()
+    plt.show()
+
+
+def d2(x, y, z):
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.set_xlabel(r'$\theta_u$')
+    ax.set_ylabel(r'$\theta_l$')
+    ax.grid(True, linestyle='-', color='0.75')
+    ax.scatter(x, y, c=z, cmap=plt.jet(), alpha=0.7, marker='x')
+    fig.tight_layout()
+    plt.show()
+
+
+def draw():
+    mode = 2
+
+    df = pd.read_csv('./py_output/fine-tune.csv')
+    x = df['theta_u'].to_numpy()
+    y = df['theta_l'].to_numpy()
+    z = df['Times'].to_numpy()
+    u = df['trend ratio'].to_numpy()
+
+    if mode == 2:
+        d2(x, y, u)
+    elif mode == 3:
+        d3(x, y, u)
+    elif mode == 4:
+        d4(x, y, z, u)
+
+
+def analyze():
     file_paths = get_files()
     with open('./py_output/fine-tune.csv', 'w') as writer:
         writer.write('Times,theta_u,theta_l,trend ratio\n')
         for fp in file_paths:
             tr = get_tr(fp)
-            fns = re.match(r'.*(1\d+)times_(0\.\d*1)_(0\.\d*1)_', fp)
+            fns = re.search(r'(\d+)times_(0\.\d*)_(0\.\d*)', fp)
             times, upper_bound, lower_bound = fns.groups()
             writer.write(times)
             writer.write(',')
@@ -58,3 +115,8 @@ if __name__ == '__main__':
             writer.write(',')
             writer.write(tr)
             writer.write('\n')
+
+
+if __name__ == '__main__':
+    analyze()
+    draw()
